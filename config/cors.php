a9385013bbd1d44c7b6be806513a874149ef892f
Pasindu Priyankara <pasinduxx@hotmail.com>
2025-11-06 15:01:46 +0530

    Update CORS configuration to enhance security and support new origins

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'https://samudradist.onimtaitsl.com',
+        'http://samudradist.onimtaitsl.com',
+    ],
 
     'allowed_origins_patterns' => [],
 
