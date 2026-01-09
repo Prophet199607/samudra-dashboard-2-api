01a23202a4753370d26cbc07fc8271bdf1d102ce
Thamalpath <thamalpathsathimantha3@gmail.com>
2026-01-09 11:31:08 +0530

    Add RolesAndPermissionsSeeder to DatabaseSeeder for initial role and permission data seeding.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
     {
         // Call all your seeders here
         $this->call([
-            UserSeeder::class
+            UserSeeder::class,
+            RolesAndPermissionsSeeder::class
         ]);
     }
 }
